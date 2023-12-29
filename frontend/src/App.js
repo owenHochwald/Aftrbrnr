e59dfd2c9a0d83d1c25a678e2cc6c2befb2b2bd4
Owen Hochwald <oh@insomniacdesign.com>
2023-12-28 19:19:08 -0500

    Add basic Timely page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,55 @@
-import './App.css';
+import { useEffect } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
-  );
+import React from 'react';
+import './App.css'
+import SearchIcon from './search.svg'
+
+const App = () => {
+
+
+    useEffect(() => {
+
+    }, []);
+
+    return (
+        <div className="app">
+            <h1>Timely</h1>
+
+            <div className="search">
+                <input
+                    placeholder='Search for something'
+                    value='Where do I find this?'
+                    // implement state later
+                    onChange={() => {}}
+                 />
+                <img 
+                    src={SearchIcon}
+                    alt='search'
+                    // implement so it calls a function that uses the text in the bar to search
+                    onClick={() => {}}
+                />
+            </div>
+
+            <div className='container'>
+                <div className='button'>
+                    {/* <button
+                        onClick={() => {}}
+                    >
+                        <h1>Start Timer</h1>
+                    </button> */}
+                    <h1>Start Timer</h1>
+                </div>
+                <div className='button'>
+                    {/* <button
+                        onClick={() => {}}
+                    >
+                        <h1>Start Timer</h1>
+                    </button> */}
+                    <h1>Pause Timer</h1>
+                </div>
+            </div>
+        </div>
+        )
 }
 
 export default App;
