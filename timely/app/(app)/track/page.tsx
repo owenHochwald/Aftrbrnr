57a2b0fee12b72b3bb5d7ecfdb11ed6ac30e256f
Owen Hochwald <oh@insomniacdesign.com>
2024-01-31 16:55:36 -0500

    feat: Daily activities

--- timely/app/(app)/track/page.tsx
+++ timely/app/(app)/track/page.tsx
@@ -5,6 +5,7 @@ import { getUserSession } from "@/lib/auth"
 import { Activity } from "@prisma/client"
 import { revalidatePath } from "next/cache"
 import { ActivityDuration } from "./duration"
+import { ArrowRight } from "lucide-react"
 
 type TimeProps = {
     startAt: string
@@ -53,12 +54,12 @@ const NewActivity = ({ activity }: NewActivityProps) => {
 
     return (
         <div>
-            <h2 className="mb-2 font-semibold">What are you working on?</h2>
+            <h2 className="text-lg mb-2 font-semibold">What are you working on?</h2>
             <form action={activity ? stopActivity : startActivity}>
                 <div className="flex items-center space-x-4">
                     <Input type="text" name="name" defaultValue={activity?.name || ""} />
                     <input type="hidden" name="id" defaultValue={activity?.id || ""} />
-                    {activity && <ActivityDuration startAt={activity.startAt}/>}
+                    {activity && <ActivityDuration startAt={activity.startAt} />}
                     <Button type="submit">{activity ? 'Stop' : 'Start'}</Button>
                 </div>
             </form>
@@ -66,7 +67,39 @@ const NewActivity = ({ activity }: NewActivityProps) => {
     )
 }
 
-const DailyActivites = () => { }
+type DailyActivitesProps = {
+    activites: Activity[]
+}
+
+const DailyActivites = ({ activites }: DailyActivitesProps) => {
+
+    return (
+        <div>
+            <h2 className="text-lg mb-2 font-semibold">Your work for today!</h2>
+            <ul>{
+                activites.map(activity => (
+                    <li className='py-2 space-x-2 flex items-center' key={activity.id}>
+                        <span className="text-md font-medium w-1/5">{activity.name}</span>
+                        <span>
+                            {new Intl.DateTimeFormat(undefined, {
+                                hour: 'numeric',
+                                minute: 'numeric',
+                            }).format(activity.startAt)}
+                        </span>
+                        <ArrowRight size={20}/>
+                        <span>
+                            {new Intl.DateTimeFormat(undefined, {
+                                hour: 'numeric',
+                                minute: 'numeric',
+                            }).format(activity.endAt || new Date())}
+                        </span>
+                    </li>
+                ))
+            }</ul>
+        </div>
+    )
+}
+
 
 export default async function TrackPage() {
     const user = await getUserSession()
@@ -78,9 +111,37 @@ export default async function TrackPage() {
         }
     })
 
+    const now = new Date();
+    const startOfToday = new Date(now.getFullYear(), now.getMonth(), now.getDate());
+    const endOfToday = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 23, 59, 59);
+
+    const dailyActivites = await prisma.activity.findMany({
+        where: {
+            tenantId: user.tenant.id,
+            userId: user.id,
+            OR: [
+                {
+                    startAt: {
+                        equals: startOfToday
+                    },
+                },
+                {
+                    endAt: {
+                        lte: endOfToday
+                    },
+                },
+            ],
+        },
+        orderBy: {
+            startAt: 'desc'
+        }
+    });
+
+
     return (
-        <main className="mx-auto container py-4">
+        <main className="mx-auto container py-4 space-y-12">
             <NewActivity activity={currentActivity} />
+            <DailyActivites activites={dailyActivites} />
         </main>
     )
 }
