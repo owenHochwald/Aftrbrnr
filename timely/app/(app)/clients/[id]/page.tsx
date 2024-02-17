e5ed6aa72e685813393d624db683cbeeaf2aabe3
Owen Hochwald <oh@insomniacdesign.com>
2024-02-17 17:15:06 -0500

    feat: add delete clients

--- timely/app/(app)/clients/[id]/page.tsx
+++ timely/app/(app)/clients/[id]/page.tsx
@@ -3,6 +3,9 @@ import { prisma } from '@/lib/prisma'
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from '@/components/ui/dropdown-menu'
 import { MoreHorizontal } from 'lucide-react'
 import Link from 'next/link'
+import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from '@/components/ui/dialog'
+import { Button } from '@/components/ui/button'
+import { redirect } from 'next/navigation'
 
 type ClientPageProps = {
     params: {
@@ -24,21 +27,56 @@ export default async function ClientPage({ params }: ClientPageProps) {
         return <div>Client not found</div>
     }
 
+    async function deleteClient() {
+        'use server'
+        if (!client) throw new Error('client not found')
+        await prisma.client.deleteMany({
+            where: {
+                tenantId: user.tenant.id,
+                id: client.id
+            }
+        })
+        redirect('/clients')
+    }
+
     return (
         <div>
             <div className="flex justify-between items-center">
                 <h1 className="text-lg font-medium mb-2">Client</h1>
-                <DropdownMenu>
-                    <DropdownMenuTrigger>
-                        <MoreHorizontal />
-                    </DropdownMenuTrigger>
-                    <DropdownMenuContent>
-                        <DropdownMenuItem>
-                            <Link href={`/clients/${client.id}/edit`}>Edit</Link>
-                        </DropdownMenuItem>
-                        <DropdownMenuItem className="text-red-500">Delete</DropdownMenuItem>
-                    </DropdownMenuContent>
-                </DropdownMenu>
+                <Dialog>
+                    <DropdownMenu>
+                        <DropdownMenuTrigger>
+                            <MoreHorizontal />
+                        </DropdownMenuTrigger>
+                        <DropdownMenuContent>
+                            <DropdownMenuItem>
+                                <Link href={`/clients/${client.id}/edit`}>Edit</Link>
+                            </DropdownMenuItem>
+                            <DialogTrigger asChild>
+                                <DropdownMenuItem className="text-red-500">
+                                    Delete
+                                </DropdownMenuItem>
+                            </DialogTrigger>
+                        </DropdownMenuContent>
+                    </DropdownMenu>
+                    <DialogContent>
+                        <DialogHeader>
+                            <DialogTitle>Delete this client?</DialogTitle>
+                            <DialogDescription>
+                                This action cannot be undone. Are you sure you want to permanently
+                                permanently delete this client?
+                            </DialogDescription>
+                        </DialogHeader>
+                        <DialogFooter>
+                            <form action={deleteClient}>
+                                <Button type="submit" variant={'destructive'}>
+                                    Delete
+                                </Button>
+                            </form>
+
+                        </DialogFooter>
+                    </DialogContent>
+                </Dialog>
             </div>
             <h3>{client.name}</h3>
         </div>
