143dbc115acbe9e99db17d13cd1535bbc7937c95
Owen Hochwald <oh@insomniacdesign.com>
2024-03-14 20:53:37 -0400

    fixed import path for navbar

--- timely/app/page.tsx
+++ timely/app/page.tsx
@@ -1,4 +1,4 @@
-import { NavBar } from '@/components/nav-bar'
+import { SignedInNavBar } from '@/components/signed-in-nav-bar'
 import { redirect } from 'next/navigation'
 
 export default function Home() {
