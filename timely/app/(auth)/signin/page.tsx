f4cc5a50cb0af457bf982fef6cece0a298b1dc1c
Owen Hochwald <oh@insomniacdesign.com>
2024-03-28 14:39:25 -0400

    feat: remake login page

--- timely/app/(auth)/signin/page.tsx
+++ timely/app/(auth)/signin/page.tsx
@@ -1,16 +1,40 @@
 import { Button } from '@/components/ui/button'
-import { signIn } from 'next-auth/react'
 import Link from 'next/link'
-import GoogleSignInButton from './GoogleSignInButton'
-// import { RegisterForm } from './form'
+import Image from 'next/image'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
 
 export default function SignInPage() {
+
   return (
-    <div className="h-screen w-screen flex justify-center items-center bg-slate-100">
-      <div className="sm:shadow-xl px-8 pb-8 pt-12 sm:bg-white rounded-xl space-y-12">
-        <h1 className="font-semibold text-2xl">Log Into Aftrbrnr</h1>
-        <GoogleSignInButton />
+    <div className="w-full lg:grid lg:min-h-[600px] lg:grid-cols-2 xl:min-h-[800px]">
+    <div className="flex items-center justify-center py-12">
+      <div className="mx-auto grid w-[350px] gap-6">
+        <div className="grid gap-2 text-center">
+          <h1 className="text-3xl font-bold">Login to Aftrbrnr</h1>
+        </div>
+        <div className="grid gap-4">
+          <Button variant="outline" className="w-full">
+            Login with Google
+          </Button>
+        </div>
+        <div className="mt-4 text-center text-sm">
+          Don&apos;t have an account?{" "}
+          <Link href="#" className="underline">
+            Sign up
+          </Link>
+        </div>
       </div>
     </div>
+    <div className="hidden bg-muted lg:block">
+      <Image
+        src="/placeholder.png"
+        alt="Image"
+        width="1920"
+        height="1080"
+        className="h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+      />
+    </div>
+  </div>
   )
 }
